3b1896d193e05dcf96533ae0e8ef8cc359a6c060
Basil Homadi <deviltearbasil@gmail.com>
2020-09-10 12:08:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // Your code here
-let dodger = document.getElementById("dodger")
-dodger.style.backgroundColor = "#FF69B4"
+// let dodger = document.getElementById("dodger")
+// dodger.style.backgroundColor = "#FF69B4"
 // console.log(dodger.style.left)
 // console.log(dodger.style.bottom)
-dodger.style.bottom = "100px"
+// dodger.style.bottom = "0px"
+
+document.addEventListener("keydown", function(e) {
+  console.log(e.key)
+})
