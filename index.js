283fe3d9b0a98366ba5dadf80bbb267075688e6a
Basil Homadi <deviltearbasil@gmail.com>
2020-09-10 12:06:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Your code here
 let dodger = document.getElementById("dodger")
 dodger.style.backgroundColor = "#FF69B4"
-console.log(dodger.style.left)
-console.log(dodger.style.bottom)
+// console.log(dodger.style.left)
+// console.log(dodger.style.bottom)
+dodger.style.bottom = "100px"
