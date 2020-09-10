2260f05b3e38abb6a6d50cc1270ff813ea10161a
Basil Homadi <deviltearbasil@gmail.com>
2020-09-10 12:16:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,10 +5,26 @@
 // console.log(dodger.style.bottom)
 // dodger.style.bottom = "0px"
 
+// document.addEventListener("keydown", function(e) {
+//   if (e.key === "ArrowLeft") {
+//     let leftNumbers = dodger.style.left.replace("px","")
+//     let left = parseInt(leftNumbers, 10)
+//     dodger.style.left = `${left - 1}px`
+//   }
+// })
+
+let dodger= document.getElementById("dodger")
+
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px","")
+  let left = parseInt(leftNumbers, 10)
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
+
 document.addEventListener("keydown", function(e) {
   if (e.key === "ArrowLeft") {
-    let leftNumbers = dodger.style.left.replace("px","")
-    let left = parseInt(leftNumbers, 10)
-    dodger.style.left = `${left - 1}px`
+    moveDodgerLeft()
   }
 })
